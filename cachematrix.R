8859d8ee7fe4c102199eea5a36dd275ecc964aac
Ignacioramos <35074744+Ignacioramos@users.noreply.github.com>
2018-01-03 19:39:06 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,7 @@
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
-    x <<- solve(y)
+    x <<- y
     m <<- NULL
   }
   get <- function() x
@@ -33,7 +33,7 @@ cacheSolve <- function(x, ...) {
     return(m)
   }
   data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
+  m <- solve(data, ...)
+  x$setInverseMatrix (m)
   m
 }
